78fb8718e0425b5de22f7c9f27367ce94875eb02
Jthiess <notaweirdemail1@gmail.com>
2025-12-29 21:28:35 -0600

    Added a manifest data lookup

--- main.py
+++ main.py
@@ -2,11 +2,14 @@ import os
 import flask
 from proxmoxer import ProxmoxAPI
 import dotenv
+import requests
 
 dotenv.load_dotenv()
-FLASK_PORT = int(os.getenv('FLASK_PORT', 5000))
-CACHE_TTL = int(os.getenv('CACHE_TTL', 30))
+FLASK_PORT = int(os.getenv('Flask_Port', 5000))
 PROXMOX_USERNAME = os.getenv('Proxmox_Username')
 PROXMOX_PASSWORD = os.getenv('Proxmox_Password')
 PROXMOX_HOST = os.getenv('Proxmox_Host')
 PROXMOX_PORT = os.getenv('Proxmox_Port')
+MINECRAFT_MANIFEST_URL = os.getenv('Minecraft_Manifest_Url', 'https://piston-meta.mojang.com/mc/game/version_manifest.json')
+
+
