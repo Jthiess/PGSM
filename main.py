7b86fbea7f40181f9599ddd86c3bc29f165c801e
Jthiess <notaweirdemail1@gmail.com>
2025-12-31 03:27:16 -0600

    Added default proxmox port

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ FLASK_PORT = int(os.getenv('Flask_Port', 5000))
 PROXMOX_USERNAME = os.getenv('Proxmox_Username')
 PROXMOX_PASSWORD = os.getenv('Proxmox_Password')
 PROXMOX_HOST = os.getenv('Proxmox_Host')
-PROXMOX_PORT = os.getenv('Proxmox_Port')
+PROXMOX_PORT = int(os.getenv('Proxmox_Port', 8006))
 MINECRAFT_MANIFEST_URL = os.getenv('Minecraft_Manifest_Url', 'https://piston-meta.mojang.com/mc/game/version_manifest.json')
 
 
